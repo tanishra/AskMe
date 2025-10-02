a401e4a9bd48a2ed89f9df0a7eb34a6bcd7ff02c
Tanish Rajput <tanishrajput9@gmail.com>
2025-10-03 00:20:27 +0530

    updated the CORS middleware

--- backend/server.py
+++ backend/server.py
@@ -12,7 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000", 
         "http://127.0.0.1:3000",
-        "https://ask-me-sigma.vercel.app",
+        "https://ask-me-tawny.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
